0a3a232ec41675397f71f419a0b5a4ed60612c5f
ebibabaei <49933290+ebibabaei@users.noreply.github.com>
2022-06-09 14:54:21 +1000

    Update index.js

--- index.js
+++ index.js
@@ -85,7 +85,8 @@ app.use('/startexperiment',startExperimentPageRoute)
 
 
 
-const port = process.env.PORT || 3000;
+//const port = process.env.PORT || 3000;
+const port = 3000;
 app.listen(port, () => {
   console.log(`listening on port ${port}...`);
 })
